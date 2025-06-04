ee172e635c71c11b55906aca1a1d8ad4f3848ac2
sjnicholson <sam@samnicholson.co.uk>
2025-06-04 23:52:16 +0100

    v

--- migrations/2023_01_10_000001_add_user_money_index.php
+++ migrations/2023_01_10_000001_add_user_money_index.php
@@ -5,11 +5,7 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-<<<<<<< HEAD
-        $schema->table('flusers', function(Blueprint $table){
-=======
         $schema->table('users', function(Blueprint $table){
->>>>>>> fdace4123fa929de0fac6bdb1e341dbeb65ba253
             $table->index('money');
         });
     },
